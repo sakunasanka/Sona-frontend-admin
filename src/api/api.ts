0e4336f90a3a6bbf49b71af30228369b2ea2b3b7
Sakuna Sanka <sakunasanka@gmail.com>
2025-10-20 21:24:26 +0530

    [dev] Fix base url issue in admin

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,9 +3,11 @@ import axios from 'axios';
 
 
 const API = axios.create({
-  baseURL: 'http://localhost:5001/api', // your backend base URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || 
+    (import.meta.env.MODE === 'development'
+      ? 'https://sona.org.lk/api'
+      : 'http://localhost:5001/api'),
 });
-
 
 // Automatically attach token from localStorage
 API.interceptors.request.use((config) => {
